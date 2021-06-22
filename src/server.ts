5bdd858b8e55904034ede48bab11c8ff5e07d507
Saionara Aguiar <saionara.aguiargomes@gmail.com>
2021-06-22 20:26:53 -0300

    uploading files second day

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express"
 import "reflect-metadata";
+import {router} from "./routes"
 
 import "./database"
-
 
 // @types/express
 const app = express();
 
+app.use(express.json())
+app.use(router);
 
 // http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));

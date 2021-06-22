d86be08e0bcc89d18f0765fc265230b38a057174
Saionara Aguiar <saionara.aguiargomes@gmail.com>
2021-06-22 10:07:36 -0300

    uploading dos arquivos

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import "reflect-metadata";
+
+import "./database"
+
+
+// @types/express
+const app = express();
+
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
